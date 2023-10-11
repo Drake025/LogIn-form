92727df7ea565eb7c0e4fd2c9d2bf05b9ee25e2f
Drake <146159721+Drake025@users.noreply.github.com>
2023-10-11 15:31:58 +0800

    Update scramble.js

--- scramble.js
+++ scramble.js
@@ -1,57 +1,68 @@
 const wordText = document.querySelector(".word"),
-hintText = document.querySelector(".hint span"),
-timeText = document.querySelector(".time b"),
-inputField = document.querySelector("input"),
-refreshBtn = document.querySelector(".refresh-word"),
-checkBtn = document.querySelector(".check-word");
+  hintText = document.querySelector(".hint span"),
+  timeText = document.querySelector(".time b"),
+  inputField = document.querySelector("input"),
+  refreshBtn = document.querySelector(".refresh-word"),
+  checkBtn = document.querySelector(".check-word");
 
-let correctWord, timer;
+let correctWord, timer, usedWords = [];
 
 const initTimer = maxTime => {
-    clearInterval(timer);
-    timer = setInterval(() => {
-        if(maxTime > 0) {
-            maxTime--;
-            return timeText.innerText = maxTime;
-        }
-        alert(`Time off! ${correctWord.toUpperCase()} was the correct word`);
-        initGame();
-    }, 1000);
-}
+  clearInterval(timer);
+  timer = setInterval(() => {
+      if (maxTime > 0) {
+          maxTime--;
+          return (timeText.innerText = maxTime);
+      }
+      alert(`Time off! ${correctWord.toUpperCase()} was the correct word`);
+      initGame();
+  }, 1000);
+};
 
 const initGame = () => {
-    initTimer(30);
-    let randomObj = words[Math.floor(Math.random() * words.length)];
-    let wordArray = randomObj.word.split("");
-    for (let i = wordArray.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
-    }
-    wordText.innerText = wordArray.join("");
-    hintText.innerText = randomObj.hint;
-    correctWord = randomObj.word.toLowerCase();;
-    inputField.value = "";
-    inputField.setAttribute("maxlength", correctWord.length);
-}
+  initTimer(30);
+  let randomObj;
+  if (usedWords.length === words.length) {
+    alert("All words have been used! Click refresh button to start again");
+      usedWords = [];
+  } else {
+      do {
+          randomObj = words[Math.floor(Math.random() * words.length)];
+      } while (usedWords.includes(randomObj.word));
+
+      usedWords.push(randomObj.word);
+
+      let wordArray = randomObj.word.split("");
+      for (let i = wordArray.length - 1; i > 0; i--) {
+          let j = Math.floor(Math.random() * (i + 1));
+          [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+      }
+      wordText.innerText = wordArray.join("");
+      hintText.innerText = randomObj.hint;
+      correctWord = randomObj.word.toLowerCase();
+      inputField.value = "";
+      inputField.setAttribute("maxlength", correctWord.length);
+  }
+};
 initGame();
 
 const checkWord = () => {
-    let userWord = inputField.value.toLowerCase();
-    if(!userWord) return alert("Please enter the word to check!");
-    if(userWord !== correctWord) return alert(`Oops! ${userWord} is not a correct word`);
-    alert(`Congrats! ${correctWord.toUpperCase()} is the correct word`);
-    initGame();
-}
+  let userWord = inputField.value.toLowerCase();
+  if (!userWord) return alert("Please enter the word to check!");
+  if (userWord !== correctWord)
+      return alert(`Oops! ${userWord} is not a correct word`);
+  alert(`Congrats! ${correctWord.toUpperCase()} is the correct word`);
+  initGame();
+};
 
 refreshBtn.addEventListener("click", initGame);
 checkBtn.addEventListener("click", checkWord);
-inputField.addEventListener('keydown', function (event) {
-  if (event.key === 'Enter') {
-      // Trigger the 'Check Word' by pressing the enter key in keyboard
+inputField.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+      // Trigger the 'Check Word' by pressing the enter key in the keyboard
       checkWord();
   }
 });
-
 function toggleDropdown() {
     var dropdown = document.getElementById("myDropdown");
     dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
@@ -104,6 +115,12 @@ function toggleDropdown() {
         gameDropdown.style.display = "none";
       }
     }
-  };
+  }
+  
+  const tryAgainBtn = document.querySelector(".try-again");
+tryAgainBtn.addEventListener("click", () => {
+  usedWords = []; // Reset used words
+  initGame();
+});
   
   
